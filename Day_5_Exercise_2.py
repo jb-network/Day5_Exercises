7278a8d17faa58b7a9fdea13ff8275181251a729
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-04 13:13:58 -0700

    Update Day_5_Exercise_2.py

--- Day_5_Exercise_2.py
+++ Day_5_Exercise_2.py
@@ -2,8 +2,7 @@
 # Day 5 Exercise 2
 
 def unique_letters (target_word):
-    norm_output = sorted(set(target_word))
-    return norm_output
+    return sorted(set(target_word))
 
 # Test data
 # print(unique_letters('entertaining'))
